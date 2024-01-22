1ab5bf4d02b1757c9fafe4a43c8b4898c28d2b41
samuelalfred3 <samuelalfrednjuguna@gmail.com>
2024-01-22 21:11:29 +0300

    Fixing errors

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,46 +1,71 @@
 #include "sort.h"
 
 /**
- * counting_sort - Sorts an array of integers in ascending order using
- * the Counting sort algorithm
- * @array: The array to be sorted
- * @size: Size of the array
+ * get_max - Find the maximum value in an array of integers.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Return: The maximum integer in the array.
+ */
+int get_max(int *array, int size)
+{
+	int max_value, i;
+
+	for (max_value = array[0], i = 1; i < size; i++)
+	{
+		if (array[i] > max_value)
+			max_value = array[i];
+	}
+
+	return (max_value);
+}
+
+/**
+ * counting_sort - Sort an array of integers in ascending order
+ * using the counting sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Prints the counting array after setting it up.
  */
 void counting_sort(int *array, size_t size)
 {
+	int *count_array, *sorted_array, max_value, i;
+
 	if (array == NULL || size < 2)
 		return;
 
-	int max = array[0];
-
-	for (size_t i = 1; i < size; ++i)
-	{
-		if (array[i] > max)
-			max = array[i];
-	}
-
-	int *counting_array = malloc(sizeof(int) * (max + 1));
-
-	if (counting_array == NULL)
+	sorted_array = malloc(sizeof(int) * size);
+	if (sorted_array == NULL)
 		return;
 
-	for (int i = 0; i <= max; ++i)
-		counting_array[i] = 0;
-
-	for (size_t i = 0; i < size; ++i)
-		counting_array[array[i]]++;
-
-	size_t index = 0;
-
-	for (int i = 0; i <= max; ++i)
+	max_value = get_max(array, size);
+	count_array = malloc(sizeof(int) * (max_value + 1));
+	if (count_array == NULL)
 	{
-		while (counting_array[i] > 0)
-		{
-			array[index++] = i;
-			counting_array[i]--;
-		}
+		free(sorted_array);
+		return;
 	}
-	print_array(counting_array, max + 1);
 
-	free(counting_array);
+	for (i = 0; i < (max_value + 1); i++)
+		count_array[i] = 0;
+
+	for (i = 0; i < (int)size; i++)
+		count_array[array[i]] += 1;
+
+	for (i = 0; i < (max_value + 1); i++)
+		count_array[i] += count_array[i - 1];
+
+	print_array(count_array, max_value + 1);
+
+	for (i = 0; i < (int)size; i++)
+	{
+		sorted_array[count_array[array[i]] - 1] = array[i];
+		count_array[array[i]] -= 1;
+	}
+	for (i = 0; i < (int)size; i++)
+		array[i] = sorted_array[i];
+
+	free(sorted_array);
+	free(count_array);
 }
--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,79 +1,79 @@
 #include "sort.h"
 
-void merge_subarr(int *subarr, int *buff, size_t front, size_t mid, size_t back);
-void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back);
-void merge_sort(int *array, size_t size);
-
 /**
- * merge_subarr - Merge two sorted subarrays into a single sorted subarray.
- * @subarr: The original array containing the subarrays to merge.
- * @buff: A buffer to store the merged subarray.
- * @front: The starting index of the first subarray.
- * @mid: The ending index of the first subarray.
- * @back: The ending index of the second subarray.
+ * merge_subarray - Merge two sorted subarrays into a single sorted subarray.
+ * @subarray: An array to sort.
+ * @buffer: A buffer to store the sorted subarray.
+ * @start: The starting index of the subarray.
+ * @mid: The middle index of the subarray.
+ * @end: The ending index of the subarray.
  */
-void merge_subarr(int *subarr, int *buff, size_t front, size_t mid, size_t back)
+void merge_subarray(int *subarray, int *buffer, size_t start, size_t mid,
+		size_t end)
 {
 	size_t i, j, k = 0;
 
 	printf("Merging...\n[left]: ");
-	print_array(subarr + front, mid - front);
+	print_array(subarray + start, mid - start);
 
 	printf("[right]: ");
-	print_array(subarr + mid, back - mid);
+	print_array(subarray + mid, end - mid);
 
-	for (i = front, j = mid; i < mid && j < back; k++)
-		buff[k] = (subarr[i] < subarr[j]) ? subarr[i++] : subarr[j++];
+	for (i = start, j = mid; i < mid && j < end; k++)
+		buffer[k] = (subarray[i] < subarray[j]) ? subarray[i++] : subarray[j++];
 	for (; i < mid; i++)
-		buff[k++] = subarr[i];
-	for (; j < back; j++)
-		buff[k++] = subarr[j];
-	for (i = front, k = 0; i < back; i++)
-		subarr[i] = buff[k++];
+		buffer[k++] = subarray[i];
+
+	for (; j < end; j++)
+		buffer[k++] = subarray[j];
+
+	for (i = start, k = 0; i < end; i++)
+		subarray[i] = buffer[k++];
 
 	printf("[Done]: ");
-	print_array(subarr + front, back - front);
+	print_array(subarray + start, end - start);
 }
 
 /**
- * merge_sort_recursive - Apply the merge sort algorithm to a subarray.
- * @subarr: The original array containing the subarray to sort.
- * @buff: A buffer to store intermediate results during sorting process.
- * @front: The starting index of the subarray to sort.
- * @back: The ending index of the subarray to sort.
+ * merge_sort_recursive - Implement the merge sort algorithm through recursion.
+ * @subarray: An array to sort.
+ * @buffer: A buffer to store the sorted result.
+ * @start: The starting index of the subarray.
+ * @end: The ending index of the subarray.
  */
-void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back)
+void merge_sort_recursive(int *subarray, int *buffer, size_t start, size_t end)
 {
 	size_t mid;
 
-	if (back - front > 1)
+	if (end - start > 1)
 	{
-		mid = front + (back - front) / 2;
-		merge_sort_recursive(subarr, buff, front, mid);
-		merge_sort_recursive(subarr, buff, mid, back);
-		merge_subarr(subarr, buff, front, mid, back);
+		mid = start + (end - start) / 2;
+		merge_sort_recursive(subarray, buffer, start, mid);
+		merge_sort_recursive(subarray, buffer, mid, end);
+		merge_subarray(subarray, buffer, start, mid, end);
 	}
 }
 
 /**
- * merge_sort - Sort an array of integers in ascending order using merge sort.
- * @array: The array of integers to sort.
+ * merge_sort - Sort an array of integers in ascending order
+ * using the merge sort algorithm.
+ * @array: An array of integers.
  * @size: The size of the array.
  *
  * Description: Implements the top-down merge sort algorithm.
  */
 void merge_sort(int *array, size_t size)
 {
-	int *buff;
+	int *buffer;
 
 	if (array == NULL || size < 2)
 		return;
 
-	buff = malloc(sizeof(int) * size);
-	if (buff == NULL)
+	buffer = malloc(sizeof(int) * size);
+	if (buffer == NULL)
 		return;
 
-	merge_sort_recursive(array, buff, 0, size);
+	merge_sort_recursive(array, buffer, 0, size);
 
-	free(buff);
+	free(buffer);
 }
